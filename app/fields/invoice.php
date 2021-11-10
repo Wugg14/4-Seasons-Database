6b6a30d7abd6bef383c9a939411efc8d0fad4c9d
Mark Steininger <mark.steininger@gmail.com>
2021-11-09 21:34:48 -0500

    invoice fields

--- app/fields/invoice.php
+++ app/fields/invoice.php
@@ -10,6 +10,20 @@ $fields = new FieldsBuilder('invoice');
 $fields->setLocation('post_type', '==', 'invoice');
 
 $fields
+    ->addDatePicker('date', [
+        'wrapper' => ['width' => '50']
+    ])
+    ->addSelect('location', [
+      'wrapper' => ['width' => '50'],
+      'choices' => ['Field', 'Office - 4 Seasons']
+    ])
+    ->addGroup('Owner Information', [
+        'description' => 'Owner information required for office visits',
+    ])
+      ->addText('owner')
+      ->addText('owner_address')
+      ->addText('phone')
+    ->endGroup()
     ->addRepeater('reports')
     ->addPostObject('post_object_field', [
         'label' => 'Post Object Field',
@@ -29,6 +43,22 @@ $fields
         'ui' => 1,
     ])
     ->addText('nickname')
+    ->addPostObject('service', [
+        'instructions' => '',
+        'required' => 0,
+        'conditional_logic' => [],
+        'wrapper' => [
+            'width' => '25',
+            'class' => '',
+            'id' => '',
+        ],
+        'post_type' => ['service'],
+        'taxonomy' => [],
+        'allow_null' => 0,
+        'multiple' => 0,
+        'return_format' => 'object',
+        'ui' => 1,
+    ])
     ->endRepeater();
 
 
